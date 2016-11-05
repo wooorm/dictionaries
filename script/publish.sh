dd68af22e1ddf61cb6b3b0739cb1324c79722a6b
Titus Wormer <tituswormer@gmail.com>
2016-11-05 15:33:40 +0100

    Use 2 spaces in shell scripts

--- script/publish.sh
+++ script/publish.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 ls dictionaries | while read -r dictionary; do
-    prefix="$(pwd)/dictionaries/$dictionary"
-    minor=$(git diff "$prefix/index.dic" "$prefix/index.aff" "$prefix/index.js")
-    patch=$(git diff "$prefix")
-    version=""
+  prefix="$(pwd)/dictionaries/$dictionary"
+  minor=$(git diff "$prefix/index.dic" "$prefix/index.aff" "$prefix/index.js")
+  patch=$(git diff "$prefix")
+  version=""
 
-    cd "$prefix" || exit
+  cd "$prefix" || exit
 
-    if [ "$minor" != "" ]; then
-        version=$(npm version minor)
-    elif [ "$patch" != "" ]; then
-        version=$(npm version patch)
-    fi
+  if [ "$minor" != "" ]; then
+    version=$(npm version minor)
+  elif [ "$patch" != "" ]; then
+    version=$(npm version patch)
+  fi
 
-    if [ "$version" != "" ]; then
-        echo "Publishing new version $version for $dictionary"
-        # npm publish
-    fi
+  if [ "$version" != "" ]; then
+    echo "Publishing new version $version for $dictionary"
+    # npm publish
+  fi
 
-    cd "../.." || exit
+  cd "../.." || exit
 done
