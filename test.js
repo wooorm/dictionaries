8cf8f6d4524d09c52d046042bdeffb7b38b0cfef
Titus Wormer <tituswormer@gmail.com>
2020-03-01 13:14:15 +0100

    Add check for canonical, normalized BCP-47 tag

--- test.js
+++ test.js
@@ -8,13 +8,27 @@ var vfile = require('to-vfile')
 var bail = require('bail')
 var not = require('not')
 var hidden = require('is-hidden')
-var isUTF8 = require('utf-8-validate')
+var isUtf8 = require('utf-8-validate')
+var normalize = require('bcp-47-normalize')
 
 var root = 'dictionaries'
 
 var checks = {
+  'Should have a canonical BCP-47 tag': bcp47,
   'All required files should exist': requiredFiles,
   'All files should be in UTF-8': utf8
+}
+
+function bcp47(name) {
+  assert.strictEqual(
+    name,
+    normalize(name, {warning: warn}),
+    name + ' should be a canonical, normal BCP-47 tag'
+  )
+
+  function warn(reason) {
+    console.log('warning:%s: %s', name, reason)
+  }
 }
 
 function utf8(name) {
@@ -26,7 +40,7 @@ function utf8(name) {
 
   function check(filename) {
     var file = vfile.readSync(path.join(dirname, filename))
-    assert.ok(isUTF8(file.contents), file.basename + ' should be utf8')
+    assert.ok(isUtf8(file.contents), file.basename + ' should be utf8')
   }
 }
 
