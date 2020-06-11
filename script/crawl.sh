7e7bd4de766cb3618a261a39e4cfcab4dbf0faba
Titus Wormer <tituswormer@gmail.com>
2020-06-11 17:16:18 +0200

    Regenerate French

--- script/crawl.sh
+++ script/crawl.sh
@@ -221,7 +221,7 @@ crawl "esperanto" \
 #   "http://www.stava.fo/download/hunspell.zip"
 crawl "french" \
   "https://grammalecte.net" \
-  "https://grammalecte.net/grammalecte/oxt/Grammalecte-fr-v1.6.0.oxt"
+  "https://grammalecte.net/grammalecte/oxt/Grammalecte-fr-v1.10.0.oxt"
 crawl "frisian" \
   "https://github.com/PanderMusubi/frisian" \
   "https://github.com/PanderMusubi/frisian/archive/master.zip"
