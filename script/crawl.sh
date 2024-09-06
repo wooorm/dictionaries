d42908c86aa97d69131032b41596ed0d7aac0c72
Alf Eaton <eaton.alf@gmail.com>
2024-09-06 22:28:34 +0100

    Use a new source for the Welsh dictionary

--- script/crawl.sh
+++ script/crawl.sh
@@ -505,8 +505,8 @@ crawl "vietnamese" \
   "https://github.com/1ec5/hunspell-vi/releases/download/v2.2.0/vi_spellchecker_OOo3.oxt"
 # Nothing.
 crawl "welsh-gb" \
-  "https://github.com/techiaith/hunspell-cy" \
-  "https://github.com/techiaith/hunspell-cy/archive/master.zip"
+  "https://github.com/fin-w/LibreOffice-Geiriadur-Cymraeg-Welsh-Dictionary" \
+  "https://github.com/fin-w/LibreOffice-Geiriadur-Cymraeg-Welsh-Dictionary/archive/master.zip"
 
 printf "$(bold "Crawled")!\n\n"
 
@@ -985,8 +985,8 @@ generate "vi" "vietnamese" \
   "dictionaries/vi_VN.aff" "UTF-8" \
   "GPL-2.0" "LICENSES-en.txt" "UTF-8"
 generate "cy" "welsh-gb" \
-  "hunspell-cy-master/cy_GB.dic" "UTF-8" \
-  "hunspell-cy-master/cy_GB.aff" "UTF-8" \
-  "LGPL-3.0" "hunspell-cy-master/LICENCE" "UTF-8"
+  "LibreOffice-Geiriadur-Cymraeg-Welsh-Dictionary-main/dictionaries/cy_GB.dic" "UTF-8" \
+  "LibreOffice-Geiriadur-Cymraeg-Welsh-Dictionary-main/dictionaries/cy_GB.aff" "UTF-8" \
+  "GPL-3.0" "LibreOffice-Geiriadur-Cymraeg-Welsh-Dictionary-main/LICENSE" "UTF-8"
 
 printf "$(bold "Generated")!\n\n"
